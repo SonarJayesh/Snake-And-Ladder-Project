29d62a30f86e7e5e90256ce683d8e6b576b26710
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-09 21:47:50 +0530

    [ADDED]: JAYESH UC-1 is Completed print Player Start Position

--- src/com/jayesh/snakeandladder/SnakeAndLadderGame.java
+++ src/com/jayesh/snakeandladder/SnakeAndLadderGame.java
@@ -1,9 +1,19 @@
 package com.jayesh.snakeandladder;
 
 public class SnakeAndLadderGame {
-
+    
+	private int playerPosition = 0;
+	
+	public void showPosition() {
+		
+		System.out.println("Player Position = "+playerPosition);
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Snake And Ladder Game");
+		
+		SnakeAndLadderGame snakeandladdergame = new SnakeAndLadderGame();
+		snakeandladdergame.showPosition();
+		
 	}
 }
