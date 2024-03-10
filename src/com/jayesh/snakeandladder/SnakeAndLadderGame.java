2966d6d3a045a1242c88a5bf1eeacd4f37fcf6e3
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-10 19:09:36 +0530

    [ADDED]: JAYESH Check the Dies Option for No_play, Ladder, Snake

--- src/com/jayesh/snakeandladder/SnakeAndLadderGame.java
+++ src/com/jayesh/snakeandladder/SnakeAndLadderGame.java
@@ -6,16 +6,43 @@ public class SnakeAndLadderGame {
     
 	private int playerPosition = 0;
 	private int rollDie = 0;
+	private int diesOption = 0;
+	private static final int NO_Play = 0;
+	private static final int LAADDER = 1;
+	private static final int SNAKE = 2;
+	
 	public void showPosition() {
 		
 		System.out.println("Player Position : " + playerPosition);
+		
 	}
 	
-	public void playGame() {
+	public void playGame(){
 		
 		Random random = new Random();
 		rollDie = random.nextInt(6)+1;
-		System.out.println("Die Throw No :"+ rollDie);
+		System.out.println("rollDie No :"+ rollDie);
+		diesOption= random.nextInt(3);
+		System.out.println("Dies Position :"+ diesOption);
+		
+		switch(diesOption)
+		{
+			case NO_Play:
+					playerPosition = playerPosition;
+					break;
+		 
+			case LAADDER:
+					playerPosition = playerPosition + rollDie; 
+					break;
+					
+			case SNAKE:
+					playerPosition = playerPosition - rollDie;
+					if(playerPosition < 0) {
+						
+						playerPosition = 0;
+					}
+					break;
+		}
 		
 	}
 	
@@ -26,6 +53,6 @@ public class SnakeAndLadderGame {
 		SnakeAndLadderGame snakeandladdergame = new SnakeAndLadderGame();
 		snakeandladdergame.showPosition();
 		snakeandladdergame.playGame();
-		
+		snakeandladdergame.showPosition();
 	}
 }
