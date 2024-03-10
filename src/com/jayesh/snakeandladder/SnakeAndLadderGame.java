6f968274168a5012dc07c353296f379c3877a130
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-10 17:35:09 +0530

    [ADDED]: JAYESH UC-2 is Completed Player Roll the Die

--- src/com/jayesh/snakeandladder/SnakeAndLadderGame.java
+++ src/com/jayesh/snakeandladder/SnakeAndLadderGame.java
@@ -1,19 +1,31 @@
 package com.jayesh.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadderGame {
     
 	private int playerPosition = 0;
-	
+	private int rollDie = 0;
 	public void showPosition() {
 		
-		System.out.println("Player Position = "+playerPosition);
+		System.out.println("Player Position : " + playerPosition);
 	}
+	
+	public void playGame() {
+		
+		Random random = new Random();
+		rollDie = random.nextInt(6)+1;
+		System.out.println("Die Throw No :"+ rollDie);
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Snake And Ladder Game");
 		
 		SnakeAndLadderGame snakeandladdergame = new SnakeAndLadderGame();
 		snakeandladdergame.showPosition();
+		snakeandladdergame.playGame();
 		
 	}
 }
