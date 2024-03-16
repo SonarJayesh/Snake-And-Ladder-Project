c1402bbf9c2c86fd5ebdb2dff59ae245e890aed6
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-16 11:48:12 +0530

    [MODIFIED]: JAYESH UC-7 is completed play 2 Player And Check Which Player Won

--- src/com/jayesh/snakeandladder/SnakeAndLadderGame.java
+++ src/com/jayesh/snakeandladder/SnakeAndLadderGame.java
@@ -5,30 +5,29 @@ import java.util.Random;
 public class SnakeAndLadderGame {
     
 	private int playerPosition = 0;
-	private int rollDie = 0;
-	private int diesOption = 0;
 	private static final int NO_Play = 0;
 	private static final int LAADDER = 1;
 	private static final int SNAKE = 2;
 	private static final int WINNING_POSITON = 100;	
-	private static int totalDie_Throw = 0;
+	private int totalDie_Throw = 0;
 	
 	public void showPosition() {
 		
-		System.out.println("Player Position : " + playerPosition);
+		System.out.println("Player Position  : " + playerPosition);
 		
 	}
 	
-	public void playGame() {
+	public int playGame() {
 		Random random = new Random();
 		
+		int rollDie = 0;
+		int diesOption = 0;
+		int totalDie_Throw = 0;
 			while(playerPosition < WINNING_POSITON) {
 			rollDie = random.nextInt(6)+1;
-			System.out.println("rollDie No :"+ rollDie);
-			diesOption= random.nextInt(3);
-			System.out.println("Dies Position :"+ diesOption);
 			
-			System.out.println("---------------------");
+			diesOption= random.nextInt(3);
+			
 			totalDie_Throw +=1;
 		switch(diesOption) {
 		
@@ -56,19 +55,55 @@ public class SnakeAndLadderGame {
 				playerPosition = playerPosition - rollDie;
 				
 			}
-			showPosition();
+				
+		}
+			
+
+			return totalDie_Throw;	
+	}
+	
+	public void playerWin( SnakeAndLadderGame snakeandladdergame2) {
+			int player1 = this.playGame();
+			int player2 = snakeandladdergame2.playGame();
+		
+			if(player1 == player2){
+				showPosition();
+				System.out.println("Total Dies Throw player1 : "+ player1);
+				System.out.println("Total Dies Throw player2 : "+ player2);
+				System.out.println("Match is Tie");
+			
+			}
+			else if(player1 < player2) {
+				showPosition();
+				System.out.println("Total Dies Throw player1 : "+ player1);
+				System.out.println("Total Dies Throw player2 : "+ player2);
+				System.out.println("Player 1st is WIN");
+			
+			}
+			else {
+				showPosition();
+				System.out.println("Total Dies Throw player1 : "+ player1);
+				System.out.println("Total Dies Throw player2 : "+ player2);
+				System.out.println("Player 2nd is WIN");
 			
 		}
-			System.out.println("Total Dies Throw :"+ totalDie_Throw);
+		
 	}
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Snake And Ladder Game");
+			System.out.println("Welcome to Snake And Ladder Game");
 		
-		SnakeAndLadderGame snakeandladdergame1 = new SnakeAndLadderGame();
-		snakeandladdergame1.showPosition();
-		snakeandladdergame1.playGame();
+			SnakeAndLadderGame snakeandladdergame1 = new SnakeAndLadderGame();
+			SnakeAndLadderGame snakeandladdergame2 = new SnakeAndLadderGame();
+		
+			System.out.println("Player 1st");
+			System.out.println("--------------------");
+
+			System.out.println("Player 2nd");
+			System.out.println("--------------------");
+			System.out.println("--------------------");
+			snakeandladdergame1.playerWin(snakeandladdergame2);
 		
 	}
 }
